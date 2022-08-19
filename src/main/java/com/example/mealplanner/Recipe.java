48fff08a5b3d4d63c1a266001716967ff6c9c84d
Alexis Seals <blarfy6216@gmail.com>
2022-08-19 16:00:43 -0600

    Fixed functionality to use database

--- src/main/java/com/example/mealplanner/Recipe.java
+++ src/main/java/com/example/mealplanner/Recipe.java
@@ -9,37 +9,35 @@ import java.util.ArrayList;
 public class Recipe {
     String recipeName;
     String recipeURL;
-    ArrayList<Item> ingredients = new ArrayList<>();
+//    ArrayList<Item> ingredients = new ArrayList<>();
 
-    public void addIngredient(Item ing){
-        ingredients.add(ing);
-    }
+//    public void addIngredient(Item ing){
+//        ingredients.add(ing);
+//    }
 
     @Override
     public String toString() {
-        String ings = "";
-        for (Item item : ingredients) {
-            ings += item.toSimpleString() + "\n";
-        }
+//        String ings = "";
+//        for (Item item : ingredients) {
+//            ings += item.toSimpleString() + "\n";
+//        }
         return "Recipe{" +
                 "recipeName='" + recipeName + '\'' +
                 ", recipeURL='" + recipeURL + '\'' +
-                ", ingredients=" + ings +
+//                ", ingredients=" + ings +
                 '}';
     }
-
-
 
     public JSONObject toJSON() {
         JSONObject jsonRecipe = new JSONObject();
         jsonRecipe.put("recipeName", recipeName);
         jsonRecipe.put("recipeURL", recipeURL);
         //TODO: put ingredients here in a prettier fashion
-        ArrayList<JSONObject> jsonIngredients = new ArrayList<>();
-        for (Item item : ingredients) {
-            jsonIngredients.add(item.toJSON());
-        }
-        jsonRecipe.put("ingredients", jsonIngredients);
+//        ArrayList<JSONObject> jsonIngredients = new ArrayList<>();
+//        for (Item item : ingredients) {
+//            jsonIngredients.add(item.toJSON());
+//        }
+//        jsonRecipe.put("ingredients", jsonIngredients);
 
         return jsonRecipe;
     }
