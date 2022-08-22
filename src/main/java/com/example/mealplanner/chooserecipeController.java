3fe7e0f8e4c78bc5e4679f779d516261c2fc1ddb
TheHanimal <calderhan@gmail.com>
2022-08-22 15:16:24 -0600

    commited coookbook files

---
+++ src/main/java/com/example/mealplanner/chooserecipeController.java
@@ -0,0 +1,40 @@
+package com.example.mealplanner;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class chooserecipeController implements Initializable {
+
+    @FXML
+    private ListView<String> myListView;
+
+    @FXML
+    private Label myRecipeLabel;
+
+    String[] recipes = {"tomato soup", "grilled cheese", "classic american dinner"};
+
+    String currentRecipe; // this might need to be something different
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        myListView.getItems().addAll(recipes);
+
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+
+                currentRecipe = myListView.getSelectionModel().getSelectedItem(); //this might have to change See line 23
+                myRecipeLabel.setText(currentRecipe);
+
+            }
+        });
+    }
+}
